ed441830ae72d331d0a85b131cffaf2116b5da5a
goreleaserbot <goreleaser@carlosbecker.com>
2021-10-16 14:56:43 -0700

    Brew formula update for pillager version v0.5.9

--- pillager.rb
+++ pillager.rb
@@ -5,25 +5,30 @@
 class Pillager < Formula
   desc "Pillage filesystems for sensitive information"
   homepage "https://github.com/brittonhayes/pillager"
-  version "0.5.8"
+  version "0.5.9"
   license "MIT"
   bottle :unneeded
 
-  if OS.mac? && Hardware::CPU.intel?
-    url "https://github.com/brittonhayes/pillager/releases/download/v0.5.8/pillager_0.5.8_Darwin_x86_64.tar.gz"
-    sha256 "7acf50e6a77b8a6c3d20155f1ab5a0f0b1bc0b2aa673dc476d545f0d8000e768"
+  on_macos do
+    if Hardware::CPU.arm?
+      url "https://github.com/brittonhayes/pillager/releases/download/v0.5.9/pillager_0.5.9_Darwin_arm64.tar.gz"
+      sha256 "2dd5d161db22b97be37fd57081455dd53a75d2d02d9ce330c72fd2728dcca8b0"
+    end
+    if Hardware::CPU.intel?
+      url "https://github.com/brittonhayes/pillager/releases/download/v0.5.9/pillager_0.5.9_Darwin_x86_64.tar.gz"
+      sha256 "0b800c9bbb6c2e9b0192639946a3ecbc9b7df4b930c385fd39e0ac3e0963aaf4"
+    end
   end
-  if OS.mac? && Hardware::CPU.arm?
-    url "https://github.com/brittonhayes/pillager/releases/download/v0.5.8/pillager_0.5.8_Darwin_arm64.tar.gz"
-    sha256 "666e10f64b78d36f1bcc9bf02f95e75be889a831a891de754c8ba7aeb25677c4"
-  end
-  if OS.linux? && Hardware::CPU.intel?
-    url "https://github.com/brittonhayes/pillager/releases/download/v0.5.8/pillager_0.5.8_Linux_x86_64.tar.gz"
-    sha256 "413d99700d354b17ec3592b93dc68b91d1eee6bb0f962fe3371289d1f631e015"
-  end
-  if OS.linux? && Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-    url "https://github.com/brittonhayes/pillager/releases/download/v0.5.8/pillager_0.5.8_Linux_arm64.tar.gz"
-    sha256 "dd9a253855bd28586ffeb863ee5c93172774210c518c854d3c370f74cb4864ea"
+
+  on_linux do
+    if Hardware::CPU.intel?
+      url "https://github.com/brittonhayes/pillager/releases/download/v0.5.9/pillager_0.5.9_Linux_x86_64.tar.gz"
+      sha256 "02a71c0da59d968eceed366e3b7a1ece9449d6b4ea946716f94eeb4f3c114a18"
+    end
+    if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
+      url "https://github.com/brittonhayes/pillager/releases/download/v0.5.9/pillager_0.5.9_Linux_arm64.tar.gz"
+      sha256 "12684a31a00a782477b49e028b39a8ac7def374b22749b4bc36726fe8d9a20c5"
+    end
   end
 
   def install
